6a441f6964286a20541e01ec615b05e706116793
NicolasLopezSTC <gclan149@gmail.com>
2023-12-12 21:37:04 -0600

    final start
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vuedirectory2/',
+  base: '/finalvue/',
   plugins: [vue()],
   resolve: {
     alias: {
